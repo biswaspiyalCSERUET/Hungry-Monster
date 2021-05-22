1109e2c557b094601130afc5a6a28b02f34dacda
biswaspiyalCSERUET <biswaspiyal777@gmail.com>
2021-05-23 05:30:54 +0600

    second commit

--- app.js
+++ app.js
@@ -1,46 +1,75 @@
-const input = document.getElementById('input-text');
-// const foodTitle = document.getElementById('card-title');
-// const cardImg = document.getElementById('card-img');
+const input = document.getElementById("input-text");
 
 const button = document.getElementById('search');
-button.addEventListener('click', function () {
+button.addEventListener("click", function () {
     const text = input.value;
-    console.log(text);
+
+    // fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    //     .then(res => res.json())
+    //     .then((data) => {
+    //         const newCategory = data.categories;
+    //         console.log(newCategory);
+    //         console.log(newCategory[0].strCategory);
+
+    //         for (let i = 0; i < newCategory.length; i++) {
+    //             const category = newCategory[i];
+    //             console.log(category.strCategory);
+    //             if (text != category.strCategory){
+    //                 alert("Please enter a valid meal name!!")
+    //             }
+    //         }
+    //         // const category = data.categories[0].strCategory;
+    //         // console.log(category);
+
+    //     })
+
 
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${text}`)
         .then(res => res.json())
         .then((data) => {
-            const newMeal = data.meals;
-            console.log(newMeal);
+            if (text.length == 0) {
+                alert("Please Enter A Meal Name");
+            } else {
+                const newMeals = data.meals;
+                const mealsDiv = document.getElementById('meals');
 
-            // let foodName = newMeal[0].strMeal;
-            // console.log(foodName);
-            // foodTitle.innerText = foodName;
+                newMeals.forEach(meal => {
+                    const mealDiv = document.createElement('div');
 
-            // let img = newMeal[0].strMealThumb;
-            // console.log(img);
-            // cardImg.src = img;
-            let row = document.getElementById('rows');
+                    const mealInfo = `
+                    <img src="${meal.strMealThumb}"/>
+                    <a onclick="printNewMeal('${meal.strMeal}')">${meal.strMeal}</a>
+                `;
 
-            for (let i = 0; i < newMeal.length; i++) {
-                let foodName = newMeal[i].strMeal;
-                console.log(foodName);
-                // foodTitle.innerText = foodName;
+                    mealDiv.innerHTML = mealInfo;
+                    mealsDiv.appendChild(mealDiv);
+                });
 
-                let img = newMeal[i].strMealThumb;
-                console.log(img);
-                // cardImg.src = img;
-
-                const image = document.createElement('img');
-                image.src = img;
-                const h5 = document.createElement('h5');
-                h5.innerText = foodName;
-                let newDiv = document.createElement('div');
-                newDiv.appendChild(image);
-                newDiv.appendChild(h5);
-                row.appendChild(newDiv);
             }
         })
-
-
 })
+
+const printNewMeal = (name) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            const newData = data.meals;
+
+            const mealDiv = document.getElementById('mealDetails');
+            mealDiv.innerHTML = `
+                <img src="${newData[0].strMealThumb}"/>
+                <h3>${newData[0].strMeal}</h3>
+                <h6>Ingredients</h6>
+                <li>${newData[0].strMeasure1}</li>
+                <li>${newData[0].strMeasure2}</li>
+                <li>${newData[0].strMeasure3}</li>
+                <li>${newData[0].strMeasure4}</li>
+                <li>${newData[0].strMeasure5}</li>
+                <li>${newData[0].strMeasure6}</li>
+                <li>${newData[0].strMeasure7}</li>
+                <li>${newData[0].strMeasure8}</li>
+            `
+        })
+}
